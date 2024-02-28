dd600f258e0b13d50033133b48b42a28d0a8efb2
Michel Momose <michelmomose@gmail.com>
2024-02-28 20:26:10 -0300

    alterações exercicio 3

--- linguagem de programação 1/src/linguagem_programacao1/exercicio3.java
+++ linguagem de programação 1/src/linguagem_programacao1/exercicio3.java
@@ -4,26 +4,20 @@ import java.util.Scanner;
 
 public class exercicio3 {
     public static void main(String[] args) {
-        // Criando um scanner para entrada de dados
         Scanner scanner = new Scanner(System.in);
 
-        // Solicitando e lendo a nota da primeira prova
         System.out.print("Digite a nota da primeira prova (0 a 10): ");
         double P1 = scanner.nextDouble();
 
-        // Solicitando e lendo a nota do primeiro exercicio
         System.out.print("Digite a nota do primeiro exercicio (0 a 10): ");
         double E1 = scanner.nextDouble();
         
-        // Solicitando e lendo a nota do segundo exercicio
         System.out.print("Digite a nota do segundo exercicio (0 a 10): ");
         double E2 = scanner.nextDouble();
         
-        // Solicitando e lendo a nota da API
         System.out.print("Digite a nota da API (0 a 10): ");
         double API = scanner.nextDouble();
         
-        // Solicitando e lendo os pontos extras
         System.out.print("Digite a nota dos pontos extras (0 a 2): ");
         double X = scanner.nextDouble();
         
@@ -31,7 +25,7 @@ public class exercicio3 {
         if (P1 < 0 || P1 > 10 || E1 < 0 || E1 > 10 
         	|| E2 < 0 || E2 > 10 || API < 0 || API > 10 || X < 0 || X > 2) {
             System.out.println("As notas devem estar no intervalo de 0 a 10.");
-            return; // Encerra o programa
+            return; 
         }
 
         // Calculando a média das notas
@@ -44,14 +38,13 @@ public class exercicio3 {
         if (mediaSemSub >= 6.0) {
         	System.out.println("Aluno aprovado!");
         } else {
-            // Solicitando e lendo a nota da prova substitutiva
             System.out.print("Digite a nota da prova substitutiva (0 a 10): ");
             double SUB = scanner.nextDouble();
             
             // Verificando se a nota da prova substitutiva está no intervalo válido
             if (SUB < 0 || SUB > 10) {
                 System.out.println("A nota da prova substitutiva deve estar no intervalo de 0 a 10.");
-                return; // Encerra o programa
+                return;
             }
             
             // Recalculando a média considerando a prova substitutiva
@@ -65,7 +58,6 @@ public class exercicio3 {
             }
         }
         
-        // Fechando o scanner
         scanner.close();
     }
 }
